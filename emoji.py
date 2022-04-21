0e228dd955d4e5a6a7d8442e1a9b66125be2d3e7
Jan <jan.weber123@googlemail.com>
2022-04-21 10:40:17 +0200

    initis

---
+++ emoji.py
@@ -0,0 +1,47 @@
+# microbit-module: emoji@0.1.0
+from microbit import Image
+
+😃 = Image.HAPPY
+😡 = Image.ANGRY
+💖 = Image.HEART
+💎 = Image.DIAMOND
+🏠 = Image.HOUSE
+
+
+boat1 = Image("05050:"
+              "05050:"
+              "05050:"
+              "99999:"
+              "09990")
+
+boat2 = Image("00000:"
+              "05050:"
+              "05050:"
+              "05050:"
+              "99999")
+
+boat3 = Image("00000:"
+              "00000:"
+              "05050:"
+              "05050:"
+              "05050")
+
+boat4 = Image("00000:"
+              "00000:"
+              "00000:"
+              "05050:"
+              "05050")
+
+boat5 = Image("00000:"
+              "00000:"
+              "00000:"
+              "00000:"
+              "05050")
+
+boat6 = Image("00000:"
+              "00000:"
+              "00000:"
+              "00000:"
+              "00000")
+
+all_boats = [boat1, boat2, boat3, boat4, boat5, boat6]
---
+++ main.py
@@ -0,0 +1,21 @@
+from microbit import *
+from emoji import *
+import os
+
+all_boats = [boat1, boat2, boat3, boat4, boat5, boat6]
+print( os.listdir())
+while True:
+    display.show(😃)
+    if accelerometer.was_gesture('shake'):
+        display.show(😡)
+        sleep(2000)
+    if button_a.was_pressed():
+        display.show(💖)
+        sleep(2000)
+    elif button_b.was_pressed():
+        display.show(all_boats, delay=200)
+        print("Hallo")
+        print(os.listdir())
+        sleep(2000)
+    sleep(100)
+    
