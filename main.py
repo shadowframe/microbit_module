d9db89b3d0fd7dd591de11e3fdcc771f258078c1
Jan <jan.weber123@googlemail.com>
2022-04-21 12:45:50 +0200

    test3

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ while True:
         sleep(2000)
     elif button_b.was_pressed():
         display.show(all_boats, delay=200)
-        print("Hallo1")
+        print("Hallo")
         print(os.listdir())
         sleep(2000)
     sleep(100)
